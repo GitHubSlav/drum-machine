2fb47b28a04c4858dbec97286b0ff622020b4c8e
Arthur McNuke <borodoslav@gmail.com>
2022-06-18 15:11:17 +0700

    Added styling for DrumPad

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styling/DrumPad.css'
 
 class DrumPad extends React.Component {
     render() {
       return (
-        <div id={this.props.id}>
+        <div id={this.props.id} className="drum-pad">
           <audio
             src={this.props.source}
             id={this.props.keyTrigger}
