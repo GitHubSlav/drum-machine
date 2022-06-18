2d527362ec53f6f312a2f353d301ca32b01060ed
Arthur McNuke <borodoslav@gmail.com>
2022-06-18 14:11:54 +0700

    Rendered VolumeSlider into the DrumMachine.

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -2,22 +2,24 @@ import React from 'react';
 import {heaterKit, pianoKit} from './audioTracks'
 import DrumBank from './DrumBank';
 import Switch from './Switch';
+import VolumeSlider from './VolumeSlider';
 
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      kit : pianoKit
+      kit : pianoKit,
+      volume: 30
     }
   }
 
-  render() {
-    
+  render() { 
     return (
       <div id="drum-machine">
         <DrumBank kit={this.state.kit}/>
         <div id="control-panel">
           <Switch text={"Power"}/>
+          <VolumeSlider value={this.state.volume}/>
           <Switch text={"Switch Kit"}/>
         </div>
       </div>
