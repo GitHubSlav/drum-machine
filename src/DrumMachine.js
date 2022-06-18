74651b7020cf1e9e78a98d0ea87bafcfc8b9dc37
Arthur McNuke <borodoslav@gmail.com>
2022-06-18 13:04:47 +0700

    Added DrumMachine class

---
+++ src/DrumMachine.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {heaterKit, pianoKit} from './audioTracks'
+
+class DrumMachine extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>I am DrumMachine</div>
+    );
+  }
+  
+}
+
+export default DrumMachine;
