b5839471ceac2ba1867999c907a3c9bbfdc55a0e
Arthur McNuke <borodoslav@gmail.com>
2022-06-20 13:31:13 +0700

    Added click animation to DrumPad

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -4,7 +4,7 @@ import './styling/DrumPad.css'
 class DrumPad extends React.Component {
   constructor(props) {
     super(props);
-
+    this.padRef = React.createRef();
     this.Play = this.Play.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
@@ -26,6 +26,11 @@ class DrumPad extends React.Component {
   Play() {
     if (this.props.isOn){
       this.props.setDisplayText(this.props.id);
+      
+      let pad = this.padRef.current;
+      pad.classList.add('pushed');
+      setTimeout(() => pad.classList.remove('pushed'), 100);
+
       const sound = document.getElementById(this.props.keyTrigger);
       sound.currentTime = 0;
       sound.volume = this.props.volume / 100;
@@ -35,7 +40,7 @@ class DrumPad extends React.Component {
 
   render() {
     return (
-      <div id={this.props.id} className="drum-pad" onClick={this.Play}>
+      <div id={this.props.id} className="drum-pad" onClick={this.Play} ref={this.padRef}>
         <audio
           src={this.props.source}
           id={this.props.keyTrigger}
